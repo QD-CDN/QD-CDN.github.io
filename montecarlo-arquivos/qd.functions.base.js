a35c4710c39bbfe8e5adc4f53a906d2d4726de40
Perillo Santana <perillo.santana@quatrodigital.com.br>
2017-02-15 15:37:59 -0200

    ...

---
+++ montecarlo-arquivos/qd.functions.base.js
@@ -0,0 +1,110 @@
+/**
+* Funções base
+*/
+String.prototype.trim||(String.prototype.trim=function(){return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,"")});
+"function"!==typeof String.prototype.replaceSpecialChars&&(String.prototype.replaceSpecialChars=function(){var b={"\u00e7":"c","\u00e6":"ae","\u0153":"oe","\u00e1":"a","\u00e9":"e","\u00ed":"i","\u00f3":"o","\u00fa":"u","\u00e0":"a","\u00e8":"e","\u00ec":"i","\u00f2":"o","\u00f9":"u","\u00e4":"a","\u00eb":"e","\u00ef":"i","\u00f6":"o","\u00fc":"u","\u00ff":"y","\u00e2":"a","\u00ea":"e","\u00ee":"i","\u00f4":"o","\u00fb":"u","\u00e5":"a","\u00e3":"a","\u00f8":"o","\u00f5":"o",u:"u","\u00c1":"A","\u00c9":"E", "\u00cd":"I","\u00d3":"O","\u00da":"U","\u00ca":"E","\u00d4":"O","\u00dc":"U","\u00c3":"A","\u00d5":"O","\u00c0":"A","\u00c7":"C"};return this.replace(/[\u00e0-\u00fa]/ig,function(a){return"undefined"!=typeof b[a]?b[a]:a})});
+Array.prototype.indexOf||(Array.prototype.indexOf=function(d,e){var a;if(null==this)throw new TypeError('"this" is null or not defined');var c=Object(this),b=c.length>>>0;if(0===b)return-1;a=+e||0;Infinity===Math.abs(a)&&(a=0);if(a>=b)return-1;for(a=Math.max(0<=a?a:b-Math.abs(a),0);a<b;){if(a in c&&c[a]===d)return a;a++}return-1});
+
+try {
+	var Common = {
+		run: function() {},
+		init: function() {},
+		ajaxStop: function() {},
+		windowOnload: function() {}
+	};
+
+	var Home = {
+		init: function() {},
+		ajaxStop: function() {},
+		windowOnload: function() {}
+	};
+
+	var Search = {
+		init: function() {},
+		ajaxStop: function() {},
+		windowOnload: function() {}
+	};
+
+	var Product = {
+		run: function() {},
+		init: function() {},
+		ajaxStop: function() {},
+		windowOnload: function() {}
+	};
+
+	var List = {
+		run: function() {},
+		init: function() {
+			Product.product360();
+		},
+		ajaxStop: function() {},
+		windowOnload: function() {},
+		product360: function() {
+			console.log('this');
+		}
+	};
+
+	var Institutional = {
+		init: function() {},
+		ajaxStop: function() {},
+		windowOnload: function() {}
+	};
+
+	var Orders = {
+		init: function() {},
+		ajaxStop: function() {},
+		windowOnload: function() {}
+	};
+}
+catch (e) {(typeof console !== "undefined" && typeof console.error === "function" && console.error("Houve um erro nos objetos. Detalhes: " + e.message)); }
+
+try {
+	(function() {
+		var body, ajaxStop, windowLoad;
+
+		Search.isSearch = $(document.body).is('.resultado-busca');
+		Search.isDepartament = $(document.body).is('.departamento');
+		Search.isCategory = $(document.body).is('.categoria');
+
+		windowLoad = function() {
+			Common.windowOnload();
+			if (body.is(".home")) Home.windowOnload();
+			else if (body.is(".resultado-busca, .departamento, .categoria")) Search.windowOnload();
+			else if (body.is(".produto")) Product.windowOnload();
+			else if (body.is(".listas")) List.windowOnload();
+			else if (body.is(".institucional")) Institutional.windowOnload();
+			else if (body.is(".orders")) Orders.windowOnload();
+		};
+
+		ajaxStop = function() {
+			Common.ajaxStop();
+			if (body.is(".home")) Home.ajaxStop();
+			else if (body.is(".resultado-busca, .departamento, .categoria")) Search.ajaxStop();
+			else if (body.is(".produto")) Product.ajaxStop();
+			else if (body.is(".listas")) List.ajaxStop();
+			else if (body.is(".institucional")) Institutional.ajaxStop();
+			else if (body.is(".orders")) Orders.ajaxStop();
+		};
+
+		$(function() {
+			body = $(document.body);
+			Common.init();
+			if (body.is(".home")) Home.init();
+			else if (body.is(".resultado-busca, .departamento, .categoria")) Search.init();
+			else if (body.is(".produto")) Product.init();
+			else if (body.is(".listas")) List.init();
+			else if (body.is(".institucional")) Institutional.init();
+			else if (body.is(".orders")) Orders.init();
+			$(document).ajaxStop(ajaxStop);
+			$(window).load(windowLoad);
+			body.addClass('jsFullLoaded');
+		});
+
+		Common.run();
+		if (location.pathname.substr(location.pathname.length - 2, 2).toLowerCase() == "/p")
+			Product.run();
+		else if (location.pathname.search(/^(\/giftlist|\/list\/)/) == 0)
+			List.run();
+	})();
+}
+catch (e) {(typeof console !== "undefined" && typeof console.error === "function" && $("body").addClass('jsFullLoaded jsFullLoadedError') && console.error("Houve um erro ao iniciar os objetos. Detalhes: " + e.message)); }
