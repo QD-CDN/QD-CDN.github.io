9cba019241ab18936ffe07965bf9e2f30ca01caf
Christian Valentin <christian@quatrodigital.com.br>
2017-02-22 12:45:23 -0300

    * Merge branch 'master' of github.com:QD-CDN/QD-CDN.github.io
    # Conflicts:
    #	foundit-arquivos/CSS.fdt.fwd.qd-bootstrap-common.css

--- montecarlo-arquivos/qd.functions.base.js
+++ montecarlo-arquivos/qd.functions.base.js
@@ -88,7 +88,6 @@ try {
 						responsive: true,
 						width: 500,
 						height: 500,
-						playSpeed: 162,
 						autoplayDirection: -1,
 						onReady: function() {
 							product360Image.mousemove(function(e){
