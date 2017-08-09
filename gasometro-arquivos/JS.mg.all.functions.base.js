16659c1491dfe3136224b4ed3250198837f033d5
Luiz Soares <luiz@quatrodigital.com.br>
2017-08-09 19:19:07 -0300

    ---

--- gasometro-arquivos/JS.mg.all.functions.base.js
+++ gasometro-arquivos/JS.mg.all.functions.base.js
@@ -10,11 +10,66 @@ try {
 			Common.amazingMenu();
 			Common.bannerResponsive();
 			Common.boxTelevendas();
+			Common.applySmartCart();
 			// Common.bannerOverlay();
 			// Common.facebookLikebox();
 		},
 		ajaxStop: function () {},
 		windowOnload: function () {},
+		applySmartCart: function() {
+			$('.header-qd-v1-cart').append('<div class="smart-cart-qd-v1-wrapper"><div class="qd-sc-wrapper"></div></div>');
+
+			$(document.body).append('<div class="smart-cart-qd-v2-wrapper"><div class="qd-sc-wrapper"></div></div>');
+
+			var wrapper = $(".qd-sc-wrapper");
+
+			$.QD_smartCart({
+				selector: wrapper,
+				dropDown:{
+					texts: {
+						linkCart: "Finalizar Compra",
+						cartTotal: '<span class="qd-infoTotalItems">Itens: #items</span><span class="qd-infoTotalValue">Total: #value</span>'
+					},
+					updateOnlyHover: false,
+					smartCheckout: true,
+					callback: function() {
+						$(".qd-ddc-wrapper3").prepend('<div class="qd-cartTitle"><h3>Meu carrinho</h3></div>');
+						wrapper.find('.qd_ddc_continueShopping').after(wrapper.find('.qd-ddc-viewCart'));
+					},
+					skuName: function(data) {
+						return data.name + ' - ' + data.skuName.replace(data.name, '');
+					},
+					callbackProductsList: function() {
+						wrapper.find(".qd-ddc-prodQtt").each(function() {
+							var $t = $(this);
+							$t.add($t.next('.qd-ddc-prodRemove')).wrapAll('<div class="qd-ddc-prodAction"></div>');
+						});
+					}
+				},
+				buyButton: {
+					buyButton: "body .prateleira:not(.qd-am) .buy-button"
+				}
+			});
+
+			// Callback do Quick View
+			window._QuatroDigital_prodBuyCallback = function(jqXHR, textStatus, prodLink, skus){
+				$.fn.simpleCart(true);
+				$(".shelf-qd-v1-buy-button-modal").modal("hide");
+				$(window).trigger("QuatroDigital.qd_bb_prod_add", [new $, skus[0] || 0]);
+			};
+
+			$('.header-qd-v1-cart-link').click(function(evt) {
+				evt.preventDefault();
+				$(document.body).toggleClass('qd-cart-show');
+
+				wrapper.height($(window).height());
+				wrapper.find(".qd-ddc-prodWrapper").css('max-height', $(window).height() - 193);
+			});
+
+			$('.qd_ddc_lightBoxClose').click(function(evt){
+				$(document.body).removeClass(Common.qdOverlayClass);
+			});
+		},		
 		bannerResponsive : function(){
 			$(".qd-banner-responsive .box-banner a").each(function(){
 				var $t = $(this);
