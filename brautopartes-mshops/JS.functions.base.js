a3984c80b034ae1cfc7dc3799ad81933c0f8aadc
Ismael Ussefatt <ismael@quatrodigital.com.br>
2017-12-13 11:57:57 -0200

    ---

--- brautopartes-mshops/JS.functions.base.js
+++ brautopartes-mshops/JS.functions.base.js
@@ -265,7 +265,7 @@ try {
 						if($data.find('.noProd').length && !($('.qd-minicart-items tr').length))
 							return;
 
-						$cartHtml = $('<div class="qd-minicart-dropdown" aria-hidden="true"><div class="qd-minicart-space"><span>Fechar</span></div><div class="qd-minicart-wrapper"><table class="qd-minicart-items"><tr><td class="qd-minicart-img"></td><td class="qd-minicart-name"></td><td class="qd-minicart-info"><span><span class="qd-minicart-price">Por: <span>---</span></span><span class="qd-minicart-remove"><i class="fa fa-times-circle"></i></span></span></td></tr></table><div class="qd-minicart-footer-wrapper"><table class="qd-minicart-footer"><tr><td class="qd-minicart-total">Total: <span>---</span></td><td><a href="/cart" class="qd-minicart-checkout"><i class="fa fa-shopping-basket"></i> Finalizar compra </a></td></tr></table></div></div></div>');
+						$cartHtml = $('<div class="qd-minicart-dropdown" aria-hidden="true"><div class="qd-minicart-space"><span>Fechar</span></div><div class="qd-minicart-wrapper"><table class="qd-minicart-items"><tr><td class="qd-minicart-img"></td><td class="qd-minicart-name"></td><td class="qd-minicart-info"><span><span class="qd-minicart-price">Por: <span>---</span></span><span class="qd-minicart-remove"><i class="fa fa-times-circle"></i></span></span></td></tr></table><div class="qd-minicart-footer-wrapper"><table class="qd-minicart-footer"><tr><td class="qd-minicart-total">Total: <span>---</span></td><td><a href="/cart" class="qd-minicart-checkout"><i class="fa fa-shopping-cart"></i> Finalizar compra </a></td></tr></table></div></div></div>');
 						var trBase = $cartHtml.find('.qd-minicart-items tr');
 
 						$data.find('ul.tbody').each(function() {
