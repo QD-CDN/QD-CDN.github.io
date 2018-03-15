f38d186f2aa8a55e7de8e6c88b8ffc8b9f4c80ba
Ismael Ussefatt <ismael@quatrodigital.com.br>
2018-03-15 11:27:45 -0300

    ---

--- _general/qd-neo.js
+++ _general/qd-neo.js
@@ -1,14 +1,11 @@
 $( document ).ready(function() {
     createSearchModal();
-
     $('.open-search-modal-qd-v1').click(function(){
-        console.log("click");
         $('#search-qd-v1-modal').modal("toggle");
     });
 });
 
 function createSearchModal() {
-    console.log('search modal');
     $('#search-qd-v1-modal').modal();
 };
 
