3d9a4b994fd15dd473295d9c7db05e5139e51f5f
Gabriel Araujo <gabrielbradoki@gmail.com>
2018-03-16 16:58:21 -0300

    ...

--- _general/qd-neo.js
+++ _general/qd-neo.js
@@ -3,6 +3,16 @@ $( document ).ready(function() {
     $('.open-search-modal-qd-v1').click(function(){
         $('#search-qd-v1-modal').modal("toggle");
     });
+
+    $(".closeFiltros").click(function () {
+        $(".filter-qd-v1").removeClass(".ativo");
+    });
+
+    $(".active.title").each(function(){
+        $(".active.title").click(function () {
+            $(this).next().toggleClass('qd-active-menu');
+        });
+    })
 });
 
 function createSearchModal() {
